13b18a8e95335966977c037dde72ccaf49804b6b
Primushko <118399900+Primushko@users.noreply.github.com>
2023-04-04 06:54:56 +0000

    Deploying to gh-pages from @ Primushko/goit-js-hw-09@258a15523db054acb61ab937569a71029f1836b1 🚀

--- 01-color-switcher.609cf524.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("data-start");document.querySelector("data-stop");let e=null;function o(){t.disabled=!0,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math,floor(Math,16777215*random()).toString(16),padStart(6,0)}`}),1e3)}t.addEventListener("click",o),stoptButton.addEventListener("click",o);
-//# sourceMappingURL=01-color-switcher.609cf524.js.map

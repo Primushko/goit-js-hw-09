c9347b4dbbc5d38509e97ef489ab6b7fafb39f9a
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-04-04 10:07:15 +0300

    робота над помилками 9.1.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,8 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, `0`)}`;    
 }
 
-const startButton = document.querySelector(`.data-start`);
-const stopButton = document.querySelector(`.data-stop`);
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector(`[data-stop]`);
 
 let intervalId = null;
 
